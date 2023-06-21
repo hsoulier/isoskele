80d0461ac89905a487dd6be89cb8e1d2126eeda5
hsoulier <hi.soulier@gmail.com>
2023-06-21 09:29:25 +0200

    📱 Responsive

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,14 +7,14 @@ type Props = {}
 
 const Navbar = (props: Props) => {
   return (
-    <header className="mt-12 md:flex relative justify-between mx-8 md:mx-32 items-end">
+    <header className="mt-12 flex flex-col md:flex-row relative items-center md:justify-between mx-8 md:mx-32 md:items-end gap-4 md:gap-0">
       <Logo />
-      <nav className="md:absolute left-1/2 bottom-2 md:-translate-x-1/2 inline-flex gap-8">
+      <nav className="md:absolute left-1/2 bottom-2 md:-translate-x-1/2 inline-flex gap-4 md:gap-8">
         <Link href="/sujet-etude">Sujets d&apos;études</Link>
         <Link href="/evenements">Evènements</Link>
         <Link href="/podcasts">Podcasts</Link>
       </nav>
-      <Button>Contact</Button>
+      <Button className="hidden md:block">Contact</Button>
     </header>
   )
 }
